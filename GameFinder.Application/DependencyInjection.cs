6578891b6f7339189a7ba9a9569112c0714ba6b5
DamianWiacek <damwiac0262@gmail.com>
2023-04-01 20:55:32 +0200

    Add project files.

---
+++ GameFinder.Application/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFinder.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services )
+        {
+            services.AddMediatR(config =>
+                config.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+
+            services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
+
+            return services;
+        }
+    }
+}
