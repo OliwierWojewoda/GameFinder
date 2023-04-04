9c8b2cf50b75c5e81099d6945b5bd25de2d99a72
DamianWiacek <damwiac0262@gmail.com>
2023-04-04 16:54:48 +0200

    Add city feature

--- source/GameFinder.Domain/Entities/City.cs
+++ source/GameFinder.Domain/Entities/City.cs
@@ -10,8 +10,20 @@ namespace GameFinder.Domain.Entities
     public class City
     {
         [Key]
-        public int CityId { get; set; }
+        public int CityId { get; private set; }
+        
+        public string Name { get; private set; }
 
-        public int Name { get; set; }
+        /// <summary>
+        /// Create new instance of City entity
+        /// </summary>
+        /// <param name="name"></param>
+        public static City New(string name)
+        {
+            return new City()
+            {
+                Name = name
+            };
+        }
     }
 }
--- source/GameFinder.Infrastructure/Migrations/20230404113310_CityNameFix.Designer.cs
+++ source/GameFinder.Infrastructure/Migrations/20230404113310_CityNameFix.Designer.cs
@@ -4,6 +4,7 @@ using GameFinder.Infrastructure.Persistance;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GameFinder.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230404113310_CityNameFix")]
+    partial class CityNameFix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
