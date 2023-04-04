389197ba108aaacc0d972ea1aafa3ffc3576871e
DamianWiacek <damwiac0262@gmail.com>
2023-04-04 20:21:30 +0200

    Handler moved to Command file

--- source/GameFinder.Application/Features/Courts/Commands/CreateCourtCommand.cs
+++ source/GameFinder.Application/Features/Courts/Commands/CreateCourtCommand.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using GameFinder.Application.Data;
+using GameFinder.Domain.Entities;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +9,27 @@ using System.Threading.Tasks;
 
 namespace GameFinder.Application.Features.Courts.Commands
 {
-    public record CreateCourtCommand(int city_Id) : IRequest<int>;
- 
+    public record CreateCourtCommand(int addressId) : IRequest<int>;
+
+
+    public class CreateCourtCommandHandler : IRequestHandler<CreateCourtCommand, int>
+    {
+        private readonly IDbContext _dbContext;
+
+        public CreateCourtCommandHandler(IDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<int> Handle(CreateCourtCommand request, CancellationToken cancellationToken)
+        {
+            var newCourt = Court.New(request.addressId);
+
+            var result = await _dbContext.Court.AddAsync(newCourt);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return result.Entity.Court_Id;
+        }
+    }
+
 }
