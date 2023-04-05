2bed95ea491b127769ecb68da31387c7db19b807
DamianWiacek <damwiac0262@gmail.com>
2023-04-05 23:38:45 +0200

    Remove ignored files

--- source/GameFinder.Presentation/Controllers/CourtController.cs
+++ source/GameFinder.Presentation/Controllers/CourtController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GameFinder.Application.Features.Courts.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,21 @@ using System.Threading.Tasks;
 
 namespace GameFinder.Presentation.Controllers
 {
-    internal class CourtController
+    [Route("api/Court")]
+    public class CourtController : ControllerBase
     {
+        private readonly IMediator _mediator;
+
+        public CourtController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("/AddCourt")]
+        public async Task<IActionResult> AddCourt([FromBody] CreateCourtCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
     }
 }
