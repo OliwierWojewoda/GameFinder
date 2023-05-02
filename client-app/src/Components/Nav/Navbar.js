bd012450e7035d3076eb5ca8aa655a8575927296
Oliwier Wojewoda <wojewoda9@interia.pl>
2023-05-02 16:32:14 +0200

    fix router reload

--- client-app/src/Components/Nav/Navbar.js
+++ client-app/src/Components/Nav/Navbar.js
@@ -4,12 +4,14 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom'
+
 
 export default function NavbarComponent() {
   return (
     <Navbar bg="light" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="#">GameFinder</Navbar.Brand>
+        <Navbar.Brand as={Link} to="#">GameFinder</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
@@ -17,9 +19,9 @@ export default function NavbarComponent() {
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/rankings">Rankings</Nav.Link>
-            <Nav.Link href="/users">Players</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/rankings">Rankings</Nav.Link>
+            <Nav.Link as={Link} to="/users">Players</Nav.Link>
           </Nav>
           <Form className="d-flex">
             <Form.Control
@@ -36,8 +38,8 @@ export default function NavbarComponent() {
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
-        <Nav.Link href="/register">Register</Nav.Link>
-        <Nav.Link href="/login">Login</Nav.Link>
+        <Nav.Link as={Link} to="/register">Register</Nav.Link>
+        <Nav.Link as={Link} to="/login">Login</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
