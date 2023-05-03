db440dcadd38af803273101584cfe0838231afa5
Oliwier Wojewoda <wojewoda9@interia.pl>
2023-05-03 17:23:21 +0200

    userslist

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -6,6 +6,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import {Route, Routes} from "react-router-dom";
 import Register from './Components/Auth/Register';
 import Login from './Components/Auth/Login';
+import UsersListComponent from './Components/UsersList/UsersListComponent';
+import RankingComponent from './Components/Rankings/Rankings'
 function App() {
   return (
     <div className="App">
@@ -13,8 +15,8 @@ function App() {
       <div className="container">
        <Routes>
         <Route path="/" element={<GameComponent></GameComponent>}/>
-        <Route path="Rankings" element={<GameComponent></GameComponent>}/>
-        <Route path="Players" element={<GameComponent></GameComponent>}/>
+        <Route path="Rankings" element={<RankingComponent></RankingComponent>}/>
+        <Route path="Users" element={<UsersListComponent></UsersListComponent>}/>
         <Route path="Register" element={<Register></Register>}/>
         <Route path="Login" element={<Login></Login>}/>
        </Routes> 
--- client-app/src/Components/GameComponent.js
+++ client-app/src/Components/GameComponent.js
@@ -16,72 +16,11 @@ const [PredictedEnd, setPredictedEnd] = useState("");
     const result = await axios.get("https://localhost:7124/GetAllGames");
     setGame(result.data);
   }
-  // async function save(event) {
-  
-  //   event.preventDefault();
-  //   try {
-  //     await axios.post("https://localhost:7195/api/Student/AddStudent", {
-        
-  //       stname: stname,
-  //       course: course,
-      
-  //     });
-  //     alert("Student Registation Successfully");
-  //         setId("");
-  //         setName("");
-  //         setCourse("");
-      
-    
-  //     Load();
-  //   } catch (err) {
-  //     alert(err);
-  //   }
-  // }
- 
-  // async function editStudent(students) {
-  //   setName(students.stname);
-  //   setCourse(students.course);
-  
-  //   setId(students.id);
-  // }
- 
-  // async function DeleteStudent(id) {
-  // await axios.delete("https://localhost:7195/api/Student/DeleteStudent/" + id);
-  //  alert("Employee deleted Successfully");
-  //  setId("");
-  //  setName("");
-  //  setCourse("");
-  //  Load();
-  // }
- 
-  // async function update(event) {
-  //   event.preventDefault();
-  //   try {
- 
-  // await axios.patch("https://localhost:7195/api/Student/UpdateStudent/"+ students.find((u) => u.id === id).id || id,
-  //       {
-  //       id: id,
-  //       stname: stname,
-  //       course: course,
- 
-  //       }
-  //     );
-  //     alert("Registation Updateddddd");
-  //     setId("");
-  //     setName("");
-  //     setCourse("");
-    
-  //     Load();
-  //   } catch (err) {
-  //     alert(err);
-  //   }
-  // }
- 
     return (
       <div>
         <h1>Games</h1>
       
-      <Table striped bordered hover variant="dark" align="center">
+      <Table striped bordered hover align="center">
         <thead>
           <tr>
             <th scope="col">Game Id</th>
