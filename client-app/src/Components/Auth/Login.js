d3be27531a4440411d999ae9b8e19132e41108c6
Oliwier Wojewoda <wojewoda9@interia.pl>
2023-05-03 12:16:47 +0200

    authentication

--- client-app/src/Components/Auth/Login.js
+++ client-app/src/Components/Auth/Login.js
@@ -1,2 +1,47 @@
-export default function Login(){
-    return <h1>Login</h1>}
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { useRef, useState, useEffect } from "react";
+import axios from "axios";
+
+
+function Login() {
+    const [password, setPwd] = useState('');
+    const [email, setEmail] = useState('');
+    const handleSubmit = async (e) => {
+        try{
+            const response = await axios.post('https://localhost:7124/Login',
+            {
+                user: {
+                  email: email,
+                  password: password
+                }},
+                {
+                    headers: { 'Content-Type': 'application/json' }
+                }              
+            ); 
+           await console.log(response); 
+        }        
+        catch(error){
+            console.log(error);
+        }
+    }
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter email" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control value={password} onChange={(e) => setPwd(e.target.value)} type="password" placeholder="Password" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
+
+export default Login;
