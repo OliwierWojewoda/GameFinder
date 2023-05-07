fca8eeee08d00cdbdc869f5423486213f0893ea6
Oliwier Wojewoda <wojewoda9@interia.pl>
2023-05-07 13:24:13 +0200

    Naming Fix

--- client-app/src/Components/Nav/Navbar.js
+++ client-app/src/Components/Nav/Navbar.js
@@ -4,7 +4,7 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link } from 'react-router-dom'
+import Link from 'react-router-dom'
 
 
 export default function NavbarComponent() {
